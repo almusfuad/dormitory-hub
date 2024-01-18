a2d2c28314872b929c34294ea4f806b1725e6014
almusfuad <almusfuad@gmail.com>
2024-01-18 15:11:03 +0600

    mail bg

--- booking/urls.py
+++ booking/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
       path('', views.BookingCreateView.as_view(), name = 'booking-create'),
       path('list/', views.BookingListCreateView.as_view(), name = 'booking-list'),
-      path('/<int:pk>/', views.BookingDetailView.as_view(), name = 'booking-detail'),
 ]
