ef29c51d8d5b8bca444540b68796b1e8769377c8
almusfuad <almusfuad@gmail.com>
2024-02-09 23:37:30 +0600

    get all data from details serializers

--- dormitory/serializers.py
+++ dormitory/serializers.py
@@ -10,7 +10,7 @@ class DormitoryListSerializer(serializers.ModelSerializer):
       location = LocationSerializer()
       class Meta:
             model = models.Dormitory
-            fields = ['id', 'name', 'location', 'image', 'dormitory_type', 'facilities', 'address', 'slug']
+            fields = '__all__'
 
 class DormitoryDetailsSerializer(serializers.ModelSerializer):
       class Meta:
